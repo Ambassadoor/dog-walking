946f598025906bc0a1d5e3c326b0acea8069a700
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-08-18 20:49:41 -0500

    Added event to show walker when pet is clicked

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,31 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (e) => {
+        const pet = e.target
+
+        const walker = walkers.find(obj => obj.id === Number(pet.dataset.walker))
+
+        if (pet.dataset.type === "pet") {
+            window.alert(`This pet is walked by ${walker.name}`)
+        }
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li
+                        data-type="pet"
+                        data-walker=${pet.walkerId}>
+                            ${pet.name}
+                    </li>`
     }
 
     petHTML += "</ul>"
