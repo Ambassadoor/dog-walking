5f3f0b7a6ca08eaf8c4b5bce5ac6e5849fcd7fca
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-08-19 18:33:31 -0500

    Added display walker for city functionality

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -2,6 +2,17 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (e) => {
+        const city = e.target
+
+        if (city.dataset.type === "city") {
+            window.alert(`${city.dataset.walker} is servicing this city`)
+        }
+    }
+)
+
 
 export const CityList = () => {
     let citiesHTML = "<ul>"
@@ -9,7 +20,8 @@ export const CityList = () => {
     for (const currentWalker of walkers) {
         citiesHTML += `
             <li
-                data-type="city">
+                data-type="city"
+                data-walker=${currentWalker.name}>
                     ${currentWalker.city}
             </li>`
     }
